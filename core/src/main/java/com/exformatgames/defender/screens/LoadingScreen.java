9d835d1482ebd58265b2748baabf485e008eab0a
exformat <exformat1988@gmail.com>
2022-09-30 20:42:45 +0600

    add LoadingScreen

---
+++ core/src/main/java/com/exformatgames/defender/screens/LoadingScreen.java
@@ -0,0 +1,53 @@
+package com.exformatgames.defender.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class LoadingScreen implements Screen {
+
+    private Game game;
+
+    private AssetManager assetManager;
+    private TextureAtlas textureAtlas;
+
+    public LoadingScreen(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
